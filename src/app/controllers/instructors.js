136d3db26c87a40b888ecc9a4fe25091aeb48464
luccaroli <luccaroli@users.noreply.github.com>
2020-06-22 18:47:00 -0300

    editando e deletando instrutores

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -46,17 +46,36 @@ module.exports = {
 
   },
   edit(req, res){
-    return
+    Instructor.find(req.params.id, function(instructor) {
+      if (!instructor) return res.send("Instructor not found!")
+
+      instructor.birth = date(instructor.birth).iso
+
+      return res.render("instructors/edit", { instructor })
+    })
+
 
   },
   put(req, res){
-    return
+    const keys = Object.keys(req.body)
+
+    for(key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields!")
+      }
+    }
+
+    Instructor.update(req.body, function() {
+      return res.redirect(`/instructors/${req.body.id}`)
+    })
 
   },
   delete(req, res){
-    return
+    Instructor.delete(req.body.id, function(){
+      return res.redirect(`/instructors`)
+    })
 
   }
 
-}
+} 
 
--- src/app/models/Instructor.js
+++ src/app/models/Instructor.js
@@ -7,7 +7,7 @@ module.exports = {
   all(callback) {
 
     db.query(`SELECT * FROM instructors`, function(err, results){
-      if (err) return res.send("erro no banco de dados!")
+      if (err) throw `erro no banco de dados! ${err}`
 
       callback(results.rows)
     })
@@ -35,7 +35,7 @@ module.exports = {
     ]
     
     db.query(query, values, function(err, results) {
-      if (err) return res.send("DataBase Erro!")
+      if (err) throw `DataBase Erro! ${err}`
 
       callback(results.rows[0])
     })
@@ -43,8 +43,42 @@ module.exports = {
 
   find(id, callback) {
     db.query(`SELECT * FROM instructors WHERE id = $1`, [id], function(err, results) {
-      if (err) return res.send("Data Base Error!")
+      if (err) throw `Data Base Error!${err}`
         callback(results.rows[0])
+    })
+  },
+  update(data, callback) {
+  const query =`
+    UPDATE instructors SET 
+      avatar_url=($1),
+      name=($2),
+      birth=($3),
+      gender=($4),
+      services=($5)
+    WHERE id = $6
+  `
+  const values = [
+    data.avatar_url,
+    data.name,
+    date(data.birth).iso,
+    data.gender,
+    data.services,
+    data.id
+  ]
+
+  db.query(query, values, function(err, results) {
+    if (err) throw `Database Error! ${err}`
+
+    callback() 
+  })
+
+  },
+
+  delete(id, callback) {
+    db.query(`DELETE FROM instructors WHERE id = $1`, [id], function(err, results) {
+      if (err) throw `Database Error! ${err}`
+
+      callback()
     })
   }
 }
