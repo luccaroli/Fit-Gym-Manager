8f7b816567e60a86b08a017ab8c9ba3b210ce859
luccaroli <luccaroli@users.noreply.github.com>
2020-06-24 03:12:31 -0300

    Algoritmo de paginação no front-end

--- public/scripts.js
+++ public/scripts.js
@@ -7,6 +7,36 @@ for (item of menuItems) {
     }
 }
 
+let totalPages = 20, 
+    selectedPages = 15,
+    pages = [],
+    oldPage
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPages + 2 
+    const pagesBeforeSelectedPage = currentPage >= selectedPages - 2 
+
+    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+        
+        if(oldPage && currentPage - oldPage > 2) {
+            pages.push("...")
+
+        }
+        if(oldPage && currentPage - oldPage == 2) {
+            pages.push(oldPage + 1)
+
+        }
+            pages.push(currentPage)
+
+            oldPage = currentPage
+    }
+
+}
+
+console.log(pages)
+
 const formDelete = document.querySelector("#form-delete")
 formDelete.addEventListener("submit", function(event) {
     const confirmation = confirm("Deseja Excluir?")
